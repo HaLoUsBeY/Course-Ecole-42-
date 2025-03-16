494d9be9460f757905eb58ad24381a5b88fc0945
42_Projects <haloztur@k1m41s05.42istanbul.com.tr>
2025-03-16 22:07:57 +0300

    ten

---
+++ pipex/sondurum/pipex.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: haloztur <haloztur@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/16 15:16:30 by haloztur          #+#    #+#             */
+/*   Updated: 2025/03/16 18:04:18 by haloztur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+static void	exec(char *cmd, char **env)
+{
+	char	**s_cmd;
+	char	*path;
+
+	if (!cmd)
+		perror("pipex: command not found");
+	s_cmd = ft_split(cmd, ' ');
+	if (!s_cmd || !s_cmd[0])
+	{
+		ft_putstr_fd("pipex: command not found: ", 2);
+		ft_putendl_fd(cmd, 2);
+		exit(127);
+	}
+	path = paths(s_cmd[0], env);
+	if (!path)
+	{
+		ft_putstr_fd("pipex: command not found: ", 2);
+		ft_putendl_fd(s_cmd[0], 2);
+		ft_free(s_cmd);
+		exit(127);
+	}
+	execve(path, s_cmd, env);
+	ft_putstr_fd("pipex: command not found: ", 2);
+	ft_putendl_fd(s_cmd[0], 2);
+	ft_free(s_cmd);
+	exit(127);
+}
+
+
+static void	child_pro(int *fd, char **av, char **ep)
+{
+	int	fds;
+
+	fds = open_file(av[1], 0);
+	if (fds == -1)
+		perror("Unable to open input_file");
+	dup2(fds, 0);
+	dup2(fd[1], 1);
+	close(fd[0]);
+	exec(av[2], ep);
+}
+
+static void	parent_pro(int *fd, char **av, char **ep)
+{
+	int	fds;
+
+	fds = open_file(av[4], 1);
+	if (fds == -1)
+		perror("Unable to open outFile");
+	dup2(fds, 1);
+	dup2(fd[0], 0);
+	close(fd[1]);
+	exec(av[3], ep);
+}
+
+int	main(int ac, char **av, char **ep)
+{
+	int		fd[2];
+	pid_t	pid;
+
+	if (ac != 5)
+	{
+		ft_putendl_fd("used: ./pipex infile cmd1 cmd2 outfile", 39);
+		return (1);
+	}
+	else
+	{
+		pipe(fd);
+		if (pipe(fd) == -1)
+			return (perror("pipe fail"), 1);
+		pid = fork();
+		if (pid == -1)
+			return (perror("fork fail"), 1);
+		if (pid == 0)
+			child_pro(fd, av, ep);
+		waitpid(pid, NULL, 0);
+		if (pid != 0)
+			parent_pro(fd, av, ep);
+	}
+	return (0);
+}
---
+++ pipex/sondurum/pipex_grade.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_grade.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: haloztur <haloztur@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/16 15:31:28 by haloztur          #+#    #+#             */
+/*   Updated: 2025/03/16 17:34:43 by haloztur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	ft_free(char **tab)
+{
+	size_t	i;
+
+	i = 0;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+}
+
+int	open_file(char *av, int a)
+{
+	int	result;
+
+	if (a == 0)
+	{
+		result = open(av, O_RDONLY, 0777);
+	}
+	if (a == 1)
+	{
+		result = open(av, O_CREAT | O_APPEND | O_WRONLY | O_TRUNC, 0777);
+	}
+	if (a == -1)
+	{
+		exit(1);
+	}
+	return (result);
+}
+
+static char	*rmpaths(char *name, char **env)
+{
+	int		i;
+	int		j;
+	char	*sub;
+
+	i = 0;
+	while (env[i])
+	{
+		j = 0;
+		while (env[i][j] && env[i][j] != '=')
+			j++;
+		sub = ft_substr(env[i], 0, j);
+		if (ft_strncmp(sub, name, j) == 0)
+		{
+			free(sub);
+			return (env[i] + j + 1);
+		}
+		free(sub);
+		i++;
+	}
+	return (NULL);
+}
+
+void	*paths(char *cmd, char **env)
+{
+	char	**get_path;
+	char	*part_path;
+	char	**s_cmd;
+	char	*file;
+	int		i;
+
+	get_path = ft_split((rmpaths("PATH", env)), ':');
+	s_cmd = ft_split(cmd, ' ');
+	i = -1;
+	while (get_path[++i])
+	{
+		part_path = ft_strjoin(get_path[i], "/");
+		file = ft_strjoin(part_path, s_cmd[0]);
+		free(part_path);
+		if (access(file, F_OK | X_OK) == 0)
+		{
+			free(s_cmd);
+			return (file);
+		}
+		free(file);
+	}
+	ft_free(get_path);
+	ft_free(s_cmd);
+	return (cmd);
+}
