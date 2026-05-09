ce037330186ca07863c564f6c46f48ad0c18e876
halousbey <halousbeyci@gmail.com>
2026-05-09 10:56:32 +0300

    cpp

--- Cpp/cpp01/ex04/main.cpp
+++ Cpp/cpp01/ex04/main.cpp
@@ -2,85 +2,68 @@
 #include <fstream>
 #include <string>
 
-std::string replaceAll(const std::string& content, const std::string& s1, const std::string& s2)
+void writeReplacedLine(const std::string& line, const std::string& s1, const std::string& s2, std::ofstream& out)
 {
-	if (s1.empty())
-		return content;
-	
-	std::string result;
-	size_t pos = 0;
-	size_t lastPos = 0;
-	
-	while ((pos = content.find(s1, lastPos)) != std::string::npos)
+    if (s1.empty())
 	{
-		result.append(content, lastPos, pos - lastPos);
-		result.append(s2);
-		lastPos = pos + s1.length();
-	}
-	result.append(content, lastPos, std::string::npos);
-	
-	return result;
+        out << line;
+        return;
+    }
+
+    size_t start = 0;
+    size_t pos;
+
+    while ((pos = line.find(s1, start)) != std::string::npos)//No Position : npos = -1
+	{
+        out << line.substr(start, pos - start);
+        out << s2;
+        start = pos + s1.length();
+    }
+    out << line.substr(start);
 }
 
 int main(int argc, char **argv)
 {
-	if (argc != 4)
+    if (argc != 4)
 	{
-		std::cerr << "Usage: " << argv[0] << " <filename> <s1> <s2>" << std::endl;
-		return 1;
-	}
-	
-	std::string filename = argv[1];
-	std::string s1 = argv[2];
-	std::string s2 = argv[3];
-	
-	if (s1.empty())
+        std::cerr << "Error: Missing or extra parameter!" << std::endl;
+        std::cerr << "Usage: ./sed_is_for_losers <dosya_adi> <s1> <s2>" << std::endl;
+        return 1;
+    }
+
+    std::string filename = argv[1];
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+
+    std::ifstream inFile(filename.c_str());//(C++98 -> .c_str())
+    if (!inFile.is_open())
 	{
-		std::cerr << "Error: s1 cannot be empty" << std::endl;
-		return 1;
-	}
-	
-	std::ifstream infile(filename.c_str());
-	if (!infile.is_open())
+        std::cerr << "Error: '" << filename << "' The file could not be opened or was not found!" << std::endl;
+        return 1;
+    }
+
+    std::string outFilename = filename + ".replace";//abc.txt + .replace
+    std::ofstream outFile(outFilename.c_str());
+    if (!outFile.is_open())
 	{
-		std::cerr << "Error: Could not open file '" << filename << "'" << std::endl;
-		return 1;
-	}
-	
-	std::string content;
-	std::string line;
-	
-	while (std::getline(infile, line))
+        std::cerr << "Error: Unable to create a new file!" << std::endl;
+        inFile.close();
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(inFile, line))//No ram yes ram 
 	{
-		content += line;
-		if (!infile.eof())
-			content += "\n";
-	}
-	
-	if (infile.bad())
-	{
-		std::cerr << "Error: Failed to read file '" << filename << "'" << std::endl;
-		infile.close();
-		return 1;
-	}
-	
-	infile.close();
-	
-	std::string result = replaceAll(content, s1, s2);
-	
-	std::string outfilename = filename + ".replace";
-	std::ofstream outfile(outfilename.c_str());
-	
-	if (!outfile.is_open())
-	{
-		std::cerr << "Error: Could not create file '" << outfilename << "'" << std::endl;
-		return 1;
-	}
-	
-	outfile << result;
-	outfile.close();
-	
-	std::cout << "File '" << outfilename << "' created successfully." << std::endl;
-	
-	return 0;
+        writeReplacedLine(line, s1, s2, outFile);
+        
+        if (!inFile.eof())
+            outFile << std::endl;//\n
+    }
+
+    inFile.close();
+    outFile.close();
+
+    std::cout << "Success! Modified text '" << outFilename << "' was saved to the file." << std::endl;
+
+    return 0;
 }
